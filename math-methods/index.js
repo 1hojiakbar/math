30932be20b33f923a47208519fe40d055a944a6e
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-04-01 22:57:54 +0500

    math methods

---
+++ math-methods/index.js
@@ -0,0 +1,29 @@
+// Math methods
+
+Math.PI; // matematik "π" bilan ishlash
+
+Math.E; // EPSILON number
+
+Math.abs; // manfiy soni musbatga o'zgartirish
+
+Math.floor; // soni pastga yaxlitlash
+
+Math.ceil; // soni tepaga yaxlitlash
+
+Math.round; // sonining kasr qismining birinchi raqami 5 bo'lsa tepaga aks holda pastga yaxlitlash
+
+Math.trunc; // soning kasir qismini ignor qilish
+
+Math.random; // berilgan sondan kichik qiymatlar bo'yicha random qilish
+
+Math.min; // sonlar ichidan eng kichikgini topish
+
+Math.max; // sonlar ichidan eng kattasini topish
+
+Math.pow; // soni darajaga oshirish
+
+Math.sign; // son musbat bo'lsa 1 manfiy bo'lsa -1 0ga teng bo'lsa 0 qaytaradi
+
+Math.sqrt; // soning ildizini chiqarish
+
+Math.cbrt; // soning kubini chiqarish
